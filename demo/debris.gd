ec10a6658520c0b61c6034902dfa0ed6d17c145a
MrLetsplay <mr.letsplay2003@gmail.com>
2026-07-05 16:18:39 +0200

    Add debris

---
+++ demo/debris.gd
@@ -0,0 +1,29 @@
+extends RigidBody3D
+
+var debris: Array[PackedScene] = [
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/platform.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/platform-planks.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/barrel.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/chest.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/crate-bottles.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/crate.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/bottle.fbx"),
+	preload("res://Assets/kenney_pirate-kit/Models/FBX format/bottle-large.fbx"),
+]
+
+func _ready() -> void:
+	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+	var d := debris[rng.randi_range(0, len(debris) - 1)]
+	add_child(d.instantiate())
+
+	self.body_entered.connect(collide)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if position.y > 0:
+		linear_velocity += get_gravity() * delta
+	else:
+		linear_velocity.y += -position.y * 0.2 - linear_velocity.y * 0.05
+
+func collide(node: Node) -> void:
+	print("DED", node)
---
+++ demo/world.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var t: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	t += delta
+
+	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+	if t > 2:
+		var debris: RigidBody3D = preload("res://debris.tscn").instantiate()
+
+		var spawn_angle = rng.randf_range(0, 2*PI)
+		var offset = Vector2.from_angle(spawn_angle) * 10
+		var pos = %Player.position + Vector3(offset.x, 0, offset.y)
+		debris.position = pos
+		debris.linear_velocity = (%Player.position - pos).normalized() * 2
+
+		add_child(debris)
+		t = 0
+
+	pass
