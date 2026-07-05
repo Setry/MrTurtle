2fd28a0382cde62a2e37d6ddd2cad7182a487c8f
MrLetsplay <mr.letsplay2003@gmail.com>
2026-07-05 15:20:37 +0200

    Improve input, Add shooting (WIP)

--- demo/player.gd
+++ demo/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 
 const SPEED = 10.0
 const ACCEL_SPEED = 0.1
-const MAX_TURN_SPEED = 0.1
-const TURN_SPEED = 0.001
+const MAX_TURN_SPEED = 0.05
+const TURN_SPEED = 0.005
 const DRAG = 0.99
 const JUMP_VELOCITY = 4.5
 
@@ -16,6 +16,11 @@ var board_values: Vector4i
 var angular_speed: float
 
 var aim_pos: Vector2
+var aim_rot: float
+
+var board_calibration: Vector4i
+
+var t: float
 
 func _ready() -> void:
 	print(GDExample.get_devices())
@@ -37,6 +42,7 @@ func _ready() -> void:
 	if not remote or not board:
 		print("Something is missing")
 		get_tree().quit(1)
+		return
 
 	board.board_received.connect(func(data: Vector4i) -> void:
 		board_values = data
@@ -44,18 +50,61 @@ func _ready() -> void:
 
 	remote.ir_received.connect(func(points: Array) -> void:
 		# FIXME: very jank
-		if len(points) == 0:
-			aim_pos = Vector2(0.5, 0.5)
+		#print(points)
+
+		var reduced_points = []
+		for p in points:
+			if p.x == 0 and p.y == 0:
+				continue
+
+			var found = false
+			for r in reduced_points:
+				if (r - p).length() < 0.01:
+					found = true
+					break
+
+			if found:
+				continue
+
+			reduced_points.push_back(p)
+
+		if len(reduced_points) != 2:
+			#aim_pos = Vector2(0.5, 0.5)
 			return
 
 		var avg = Vector2()
-		for p in points:
+		for p in reduced_points:
 			avg += p
-		avg /= len(points)
-		aim_pos = avg
+		avg /= len(reduced_points)
+
+		var line_len_x = abs(reduced_points[0].x - reduced_points[1].x)
+		var line_len_y = abs(reduced_points[0].y - reduced_points[1].y)
+
+		#print(line_len_x, "/", line_len_y)
+
+		var r_x = remap(avg.x, 0.5 * line_len_x, 1 - 0.5 * line_len_x, 0, 1)
+		var r_y = remap(avg.y, 0.5 * line_len_y, 1 - 0.5 * line_len_y, 0, 1)
+
+		var angle = atan2(reduced_points[0].y - reduced_points[1].y, reduced_points[0].x - reduced_points[1].x)
+		%Ui/Cursor.rotation = angle
+
+		aim_pos = Vector2(1 - r_x, r_y)
+		aim_rot = angle
+		#print(reduced_points, avg, aim_pos)
+	)
+
+	remote.key_received.connect(func(key: String, pressed: bool) -> void:
+		print(key, ": ", pressed)
+		if key == "b" and pressed:
+			shoot()
 	)
 
 func _physics_process(delta: float) -> void:
+	t += delta
+	if t < 3:
+		board_calibration = board_values
+		print("Calibrating: ", board_calibration)
+
 	# Add the gravity.
 
 	if position.y > 0:
@@ -65,20 +114,22 @@ func _physics_process(delta: float) -> void:
 
 	#print(board_values)
 
-	var left_sum = max(1, board_values.z + board_values.w)
-	var right_sum = max(1, board_values.x + board_values.y)
+	var board_v = board_values - board_calibration
+	#print(board_v)
+	var left_sum = max(1, board_v.z + board_v.w)
+	var right_sum = max(1, board_v.x + board_v.y)
 
-	var up_sum = max(1, board_values.x + board_values.z)
-	var down_sum = max(1, board_values.y + board_values.w)
+	var up_sum = max(1, board_v.x + board_v.z)
+	var down_sum = max(1, board_v.y + board_v.w)
 
 	# Normalize to -1..1
 	var lr = (float(right_sum) / (left_sum + right_sum) - 0.5) * 2
 	#print("L/R: ", lr)
 
-	var ud = (float(up_sum) / (up_sum + down_sum) - 0.25) * 2
+	var ud = (float(up_sum) / (up_sum + down_sum) - 0.33) * 2
 	#print("U/D: ", ud)
 
-	if left_sum + right_sum < 2000:
+	if left_sum + right_sum < 200:
 		lr = 0
 		ud = 0
 
@@ -120,3 +171,20 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta: float) -> void:
 	%Ui/Cursor.position = %Ui/Cursor.position.move_toward(aim_pos * get_viewport().get_visible_rect().size, 50)
+
+func shoot() -> void:
+	var turt = preload("res://bubble.tscn")
+	var i_turt: RigidBody3D = turt.instantiate()
+
+	var shoot_pos = position + transform.basis * Vector3(aim_pos.x * 10, aim_pos.y * 10, -1)
+	var shoot_speed = transform.basis * Vector3(0, 0, -10)
+
+	var screen_position = aim_pos * get_viewport().get_visible_rect().size
+	print(get_viewport().get_screen_transform().affine_inverse() * screen_position)
+
+	%Camera.project_position()
+	i_turt.position = shoot_pos
+	i_turt.linear_velocity = shoot_speed
+
+	get_parent().add_child(i_turt)
+	pass
